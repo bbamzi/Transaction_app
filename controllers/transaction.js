705f02dfa7975fbf44bb06b373c11e66fbcf216d
Akinbode Bamigboye <bodejuniorforever@gmail.com>
2022-07-14 18:44:57 +0100

    workid on mongodb

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -7,28 +7,58 @@ exports.getAddTransaction = (req, res, next) => {
 };
 
 exports.postAddTransaction = (req, res, next) => {
-  const serviceType = req.body.serviceType;
-  const logo = req.body.logo;
-  const brandName = req.body.brandName;
-  const dateIssued = req.body.dateIssued;
-  const dueDate = req.body.dueDate;
-  const documentNumber = req.body.documentNumber;
-  const recipientName = req.body.recipientName;
-  const billTo = req.body.billTo;
-  const paymentMethod = req.body.paymentMethod;
-  const paymentIfOther = req.body.paymentIfOther;
-  const invoicePaymentDetails = req.body.invoicePaymentDetails;
-  const description = req.body.description;
-  const unit = req.body.unit;
-  const price = req.body.price;
-  const currency = req.body.currency;
-  const shippingFee = req.body.shippingFee;
-  const vat = req.body.vat;
-  const discount = req.body.discount;
-  const sub_total = req.body.sub_total;
-  const total = req.body.total;
-  const clientSignature = req.body.clientSignature;
-
+  const {
+    serviceType,
+    logo,
+    brandName,
+    dateIssued,
+    dueDate,
+    documentNumber,
+    recipientName,
+    billTo,
+    paymentMethod,
+    paymentIfOther,
+    invoicePaymentDetails,
+    description,
+    unit,
+    price,
+    currency,
+    shippingFee,
+    vat,
+    discount,
+    sub_total,
+    total,
+    clientSignature,
+  } = req.body;
+  // const serviceType = req.body.serviceType;
+  // const logo = req.body.logo;
+  // const brandName = req.body.brandName;
+  // const dateIssued = req.body.dateIssued;
+  // const dueDate = req.body.dueDate;
+  // const documentNumber = req.body.documentNumber;
+  // const recipientName = req.body.recipientName;
+  // const billTo = req.body.billTo;
+  // const paymentMethod = req.body.paymentMethod;
+  // const paymentIfOther = req.body.paymentIfOther;
+  // const invoicePaymentDetails = req.body.invoicePaymentDetails;
+  // const description = req.body.description;
+  // const unit = req.body.unit;
+  // const price = req.body.price;
+  // const currency = req.body.currency;
+  // const shippingFee = req.body.shippingFee;
+  // const vat = req.body.vat;
+  // const discount = req.body.discount;
+  // const sub_total = req.body.sub_total;
+  // const total = req.body.total;
+  // const clientSignature = req.body.clientSignature;
+  const listItems = [description, unit, price];
+  listItems.forEach((ele) => {});
+  let itemsObj = {
+    description: String(description),
+    unit: +unit,
+    price: +price,
+  };
+  console.log(listItems);
   const transaction = new Transaction({
     serviceType,
     logo,
@@ -41,7 +71,7 @@ exports.postAddTransaction = (req, res, next) => {
     paymentMethod,
     paymentIfOther,
     invoicePaymentDetails: invoicePaymentDetails.toString(),
-    items: ,
+    items: itemsObj,
     currency,
     shippingFee,
     vat,
@@ -50,17 +80,18 @@ exports.postAddTransaction = (req, res, next) => {
     total,
     clientSignature,
   });
-  console.log("working");
+  console.log(transaction);
+
   console.log(req.body);
-  transaction
-    .save()
-    .then((result) => {
-      console.log("Created Transaction");
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // transaction
+  //   .save()
+  //   .then((result) => {
+  //     console.log("Created Transaction");
+  //     res.redirect("/");
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 };
 
 const transactions = [];
