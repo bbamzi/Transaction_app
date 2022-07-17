a05a838be2a5753d5ab638d071162ad8c894d441
Akinbode Bamigboye <bodejuniorforever@gmail.com>
2022-07-17 04:42:55 +0100

    items populate feture for model

--- app.js
+++ app.js
@@ -28,3 +28,4 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+// app.listen(3000);
--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,8 @@ const items = document.querySelector(".items");
 const selectReceiptBtn = document.querySelector(".receipt");
 const selectInvoiceBtn = document.querySelector(".invoice");
 const serviceType = document.querySelector("#service_type_hidden");
+const checkbox = document.querySelector("input[name=autoGenerate]");
+// const autoGenerate = document.getElementById("autoGenerate");
 
 addItemsBtn.addEventListener("click", function (e) {
   e.preventDefault();
@@ -14,7 +16,7 @@ addItemsBtn.addEventListener("click", function (e) {
   <label>Description</label>
   <textarea name="description" cols="20" rows="2"></textarea>
   <label>Unit/Quantity</label>
-  <input name="Unit"/>
+  <input name="unit"/>
   <label>Price</label>
   <input type="text" name="price"/><br/>
 </div>`;
@@ -43,4 +45,20 @@ selectReceiptBtn.addEventListener("click", function (e) {
       item.style.display = "none";
     });
   });
+});
+
+checkbox.addEventListener("change", function () {
+  let brandName = document.getElementById("brand_name").value;
+  if (this.checked) {
+    let nums =
+      brandName
+        .toLowerCase()
+        .split(" ")
+        .map((e) => e[0])
+        .join("") + "0001";
+    document.getElementById("documentNumber").value =
+      nums[0].toUpperCase() + nums.slice(1);
+  } else {
+    document.getElementById("documentNumber").value = "";
+  }
 });
